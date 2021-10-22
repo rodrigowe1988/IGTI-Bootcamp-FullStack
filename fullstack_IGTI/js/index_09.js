4c65b404dc24c4b34809c05e07616fc7876d1ffc
Rodrigo Weber <rodrigoweber97@hotmail.com>
2021-10-22 12:07:04 -0300

    adding_new_files

---
+++ fullstack_IGTI/js/index_09.js
@@ -0,0 +1,5 @@
+//concatenar string com template literals
+const fullName = (first, last) => `${first} ${last}`;
+
+console.log(fullName("Rodrigo", "Weber"));
+console.log(fullName("Michele", "Dutra"));
