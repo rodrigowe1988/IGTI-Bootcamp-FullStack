dffb3053e326e599a4fc36824e943311ca843156
Rodrigo Weber <rodrigoweber@pop-os.localdomain>
2022-08-08 19:29:33 -0300

    working with js events

--- fundaments_javascript/forms/script.js
+++ fundaments_javascript/forms/script.js
@@ -1,11 +1,34 @@
 console.log("03 -> cap 03");
 
-let input = document.querySelector("#input01");
-input.value = "Rodrigo";
+let inputName = document.querySelector("#nameInput");
+inputName.value = "Rodrigo";
 
 let theButton = document.getElementById("submitButton");
 let hiddenText = document.getElementById("hiddenText");
 
-theButton.addEventListener("click", event => {
-	hiddenText.innerHTML = 'Show and disappear'
+theButton.addEventListener("click", (event) => {
+  hiddenText.innerHTML = "Show and disappear";
 });
+
+addEventListener("load", start);
+
+function start() {
+  let span = document.querySelector("#nameLength");
+  span.textContent = "Char counter";
+  inputName.addEventListener("keyup", countChars);
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", preventSubmit);
+}
+
+function countChars(event) {
+  let count = event.target.value;
+  let span = document.querySelector("#nameLength");
+  //let totalString = inputName.value;
+  span.textContent = count.length;
+}
+
+function preventSubmit() {
+  let inputName = document.querySelector("#nameInput");
+  alert(inputName.value + ' cadastrado com sucesso!')
+}
